a99d17a692422fe708ddfa8bb4add3888eb755f9
Jason Stajich <jasonstajich.phd@gmail.com>
2017-05-18 09:59:21 -0700

    move result files in subfolder on completion

--- SRP078379/variant_jobs/07_snpEff.sh
+++ SRP078379/variant_jobs/07_snpEff.sh
@@ -4,3 +4,5 @@ MEM=64G
 module load snpEff
 
 zcat vcf/C_auris.genotypes.selected.SNPONLY.vcf.gz | java -Xmx$MEM -jar $SNPEFFJAR ann -c snpEff.config Candida_auris_gca_001189475 > gene_variant_analysis/snpEff_annot.gff3
+
+ mv snpEff_genes.txt snpEff_summary.html  gene_variant_analysis/
