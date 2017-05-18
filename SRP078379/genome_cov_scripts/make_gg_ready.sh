02941393f8fe467b0b75b60212b686edaa448bea
Jason Stajich <jasonstajich.phd@gmail.com>
2017-05-18 10:06:03 -0700

    move to sub-folder for the table files

--- SRP078379/genome_cov_scripts/make_gg_ready.sh
+++ SRP078379/genome_cov_scripts/make_gg_ready.sh
@@ -1,5 +1,5 @@
-for file in plot/*.gene_cov_norm.tab
+for file in plot/tables/*.gene_cov_norm.tab
 do
 	b=$(basename $file .tab)
-	perl genome_cov_scripts/prep_for_ggplot.pl $file > plot/$b.gg.csv
+	perl genome_cov_scripts/prep_for_ggplot.pl $file > plot/tables/$b.gg.csv
 done
