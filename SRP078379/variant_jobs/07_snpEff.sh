945684907b8e74f489a0bfed58a9980cbd8ac360
Jason Stajich <jasonstajich.phd@gmail.com>
2017-05-18 11:37:24 -0700

    update snpEff call

--- SRP078379/variant_jobs/07_snpEff.sh
+++ SRP078379/variant_jobs/07_snpEff.sh
@@ -2,7 +2,8 @@
 #SBATCH --time 2:00:00 --mem 64G -p short
 MEM=64G
 module load snpEff
-
-zcat vcf/C_auris.genotypes.selected.SNPONLY.vcf.gz | java -Xmx$MEM -jar $SNPEFFJAR ann -c snpEff.config Candida_auris_gca_001189475 > gene_variant_analysis/snpEff_annot.gff3
+module load tabix
+zcat vcf/C_auris.genotypes.selected.SNPONLY.vcf.gz | java -Xmx$MEM -jar $SNPEFFJAR ann -c snpEff.config Candida_auris_gca_001189475 > gene_variant_analysis/snpEff_annot.vcf
+bgzip gene_variant_analysis/snpEff_annot.vcf
 
  mv snpEff_genes.txt snpEff_summary.html  gene_variant_analysis/
